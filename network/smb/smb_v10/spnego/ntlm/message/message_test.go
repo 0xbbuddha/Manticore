43fc6e860db800b9dd057338ebc1701609a40356
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-06-04 21:04:37 +0200

    Created interface NTLMSSPMessage

---
+++ network/smb/smb_v10/spnego/ntlm/message/message_test.go
@@ -0,0 +1,17 @@
+package message_test
+
+import (
+	"testing"
+
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/spnego/ntlm/message"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/spnego/ntlm/message/authenticate"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/spnego/ntlm/message/challenge"
+	"github.com/TheManticoreProject/Manticore/network/smb/smb_v10/spnego/ntlm/message/negotiate"
+)
+
+func TestMessageInterface(t *testing.T) {
+	// Test that all message types implement the Message interface
+	var _ message.NTLMSSPMessage = &negotiate.NegotiateMessage{}
+	var _ message.NTLMSSPMessage = &challenge.ChallengeMessage{}
+	var _ message.NTLMSSPMessage = &authenticate.AuthenticateMessage{}
+}
