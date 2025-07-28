24b93d693590ab635aca2d9236211a83b86ba249
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-07-28 08:08:51 +0200

    Improved test cases for NTLMv1

--- crypto/ntlmv1/ntlmv1_ctx_test.go
+++ crypto/ntlmv1/ntlmv1_ctx_test.go
@@ -2,10 +2,11 @@ package ntlmv1
 
 import (
 	"encoding/hex"
+	"strings"
 	"testing"
 )
 
-func TestNTLMv1HashFromPassword(t *testing.T) {
+func Test_NTLMv1_FromPassword(t *testing.T) {
 	serverChallenge := [8]byte{0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88}
 	tests := []struct {
 		domain             string
@@ -17,11 +18,11 @@ func TestNTLMv1HashFromPassword(t *testing.T) {
 	}{
 		{
 			domain:             "WORKGROUP",
-			username:           "podalirius",
-			password:           "Podalirius!",
+			username:           "Podalirius",
+			password:           "Manticore",
 			challenge:          serverChallenge,
-			expectedNTResponse: "8110779a47517b1e6bd686317bd8bc395a07a640ad9e3e70",
-			expectedLMResponse: "f8ccbfc0d1eee6e3e6992565aaa23cc5cb0150e6034bcd28",
+			expectedNTResponse: "3675B7303C4D92F35977DBAE54CBB3198A7DF488AEECDEB9",
+			expectedLMResponse: "AAB5612B2A1AC5B983EC11EBC2D33CD54BB8CF8C3E87C69A",
 		},
 	}
 
@@ -37,13 +38,13 @@ func TestNTLMv1HashFromPassword(t *testing.T) {
 		}
 		ntResponse := response.GetNtChallengeResponse()
 		hexNTResponse := hex.EncodeToString(ntResponse[:])
-		if hexNTResponse != test.expectedNTResponse {
+		if !strings.EqualFold(hexNTResponse, test.expectedNTResponse) {
 			t.Errorf("ntlmv1Ctx.ComputeResponse() = %s; expected %s", hexNTResponse, test.expectedNTResponse)
 		}
 
 		lmResponse := response.GetLmChallengeResponse()
 		hexLMResponse := hex.EncodeToString(lmResponse[:])
-		if hexLMResponse != test.expectedLMResponse {
+		if !strings.EqualFold(hexLMResponse, test.expectedLMResponse) {
 			t.Errorf("ntlmv1Ctx.LMResponse() = %s; expected %s", hexLMResponse, test.expectedLMResponse)
 		}
 	}
@@ -53,3 +54,54 @@ func TestNTLMv1HashFromPassword(t *testing.T) {
 // [SMB] NTLMv1-SSP Username : THINKPAD-X61\user
 // [SMB] NTLMv1-SSP Password : user
 // [SMB] NTLMv1-SSP Hash     : user::THINKPAD-X61:82D6653B1699997800000000000000000000000000000000:2C84F06960FE5F2687E6D42E181F10F324A70D055A511AF7:1122334455667788
+
+func Test_NTLMv1_ToHashcatString(t *testing.T) {
+	testCases := []struct {
+		name           string
+		domain         string
+		username       string
+		password       string
+		challenge      [8]byte
+		expectedString string
+	}{
+		{
+			name:           "Basic NTLMv1 Hashcat String",
+			domain:         "THINKPAD-X61",
+			username:       "user",
+			password:       "",
+			challenge:      [8]byte{0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88},
+			expectedString: "user::THINKPAD-X61:52d536dbcefa63b9101f9c7a9d0743882f85252cc731bb25:eefabc742621a883aec4b24e0f7fbf05e17dc2880abe07cc:1122334455667788",
+		},
+		{
+			name:           "Empty Domain NTLMv1 Hashcat String",
+			domain:         "THINKPAD-X61",
+			username:       "Podalirius",
+			password:       "Manticore",
+			challenge:      [8]byte{0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88},
+			expectedString: "Podalirius::THINKPAD-X61:AAB5612B2A1AC5B983EC11EBC2D33CD54BB8CF8C3E87C69A:3675B7303C4D92F35977DBAE54CBB3198A7DF488AEECDEB9:1122334455667788",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ntlmv1Ctx, err := NewNTLMv1CtxWithPassword(tc.domain, tc.username, tc.password, tc.challenge)
+			if err != nil {
+				t.Fatalf("NewNTLMv1CtxWithPassword() error = %v", err)
+			}
+
+			response, err := ntlmv1Ctx.ComputeResponse()
+			if err != nil {
+				t.Fatalf("ComputeResponse() error = %v", err)
+			}
+
+			hashcatString, err := response.HashcatString()
+			if err != nil {
+				t.Fatalf("HashcatString() error = %v", err)
+			}
+
+			if !strings.EqualFold(hashcatString, tc.expectedString) {
+				t.Errorf("HashcatString()\n\tgot  : %v\n\twant : %v", strings.ToUpper(hashcatString), strings.ToUpper(tc.expectedString))
+			}
+		})
+	}
+}
