2f03af7fc15f217f5dbf50a4e2febd0802ed4327
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-10-31 10:53:08 +0100

    (*KeyCredential).Unmarshal should return errors but never panic, fixes #34

--- windows/keycredential/KeyCredential_test.go
+++ windows/keycredential/KeyCredential_test.go
@@ -1,1 +1,54 @@
 package keycredential_test
+
+import (
+	"testing"
+
+	"github.com/TheManticoreProject/Manticore/network/ldap"
+	"github.com/TheManticoreProject/Manticore/windows/guid"
+	"github.com/TheManticoreProject/Manticore/windows/keycredential"
+	"github.com/TheManticoreProject/Manticore/windows/keycredential/crypto"
+	"github.com/TheManticoreProject/Manticore/windows/keycredential/utils"
+	"github.com/TheManticoreProject/Manticore/windows/keycredential/version"
+)
+
+func TestKeyCredential_Unmarshal(t *testing.T) {
+	tests := []struct {
+		name                       string
+		msDsKeyCredentialLinkValue string
+		expectedKC                 *keycredential.KeyCredential
+		wantErr                    bool
+	}{
+		{
+			name:                       "Valid KeyCredential with specific identifier",
+			msDsKeyCredentialLinkValue: "B:10:9012345678:CN=POC,CN=Computers,DC=MANTICORE,DC=local",
+			expectedKC:                 nil,
+			wantErr:                    true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			kc := keycredential.NewKeyCredential(
+				version.KeyCredentialVersion{Value: version.KeyCredentialVersion_1},
+				"",
+				crypto.RSAKeyMaterial{},
+				guid.GUID{},
+				utils.DateTime{},
+				utils.DateTime{},
+			)
+
+			dnb := ldap.DNWithBinary{}
+			err := dnb.Parse([]byte(tt.msDsKeyCredentialLinkValue))
+			if err != nil {
+				t.Errorf("Parse() error = %v", err)
+				return
+			}
+
+			_, err = kc.Unmarshal(dnb.BinaryData)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Unmarshal() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
