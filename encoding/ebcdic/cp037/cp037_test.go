c854921f6c00b289ad585da4ac9483f315896a27
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-12-26 17:41:52 +0100

    Created ebcdic cp037

---
+++ encoding/ebcdic/cp037/cp037_test.go
@@ -0,0 +1,20 @@
+package cp037_test
+
+import (
+	"testing"
+
+	"github.com/TheManticoreProject/Manticore/encoding/ascii"
+	"github.com/TheManticoreProject/Manticore/encoding/ebcdic/cp037"
+)
+
+func TestCp037ToAsciiMap(t *testing.T) {
+	result := cp037.EbcdicCp037ToAscii(ascii.ASCIICharset[:])
+	if len(result) != len(cp037.EbcdicCp037ToAsciiMap[:]) {
+		t.Fatalf("Result length mismatch, got %d expected %d", len(result), len(cp037.EbcdicCp037ToAsciiMap[:]))
+	}
+	for i, v := range cp037.EbcdicCp037ToAsciiMap[:] {
+		if result[i] != v {
+			t.Errorf("Ebcdiccp037ToAscii failed at index %d: got %v, expected %v", i, result[i], v)
+		}
+	}
+}
